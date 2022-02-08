a6c7c7d3363216e33cd95ebe6e4919780b0d1411
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-08 10:24:39 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+      <div>
+          <h2>App</h2>
+          <button className="btn brn-primary">Button</button>
+      </div>
+  );
+};
+
+export default App;
